82c69d628004ae8bbab7c8e18d2c6210834c07b9
kyak <bas@bmail.ru>
2013-06-20 21:57:10 +0400

    Always use MLink to download to target in PIL mode

--- microdaq/microdaq_download.m
+++ microdaq/microdaq_download.m
@@ -5,28 +5,30 @@ disp(['### Downloading ', modelName, ' to MicroDAQ...']);
 TargetRoot = getpref('microdaq','TargetRoot');
 CCSRoot = getpref('microdaq','CCSRoot');
 
+%MLink is a:
+% default download method and
+% always used in PIL
+downloadMethod = '"MLink"';
 if (ischar(makertwObj)) %String 'PIL'
     outfile = modelName;
 else
     outfile = fullfile(makertwObj.BuildDirectory, [modelName, '.out']);
-end
-
-% Check the MicroDAQ download method
-if verLessThan('matlab', '8.1')
-    makertwObj = get_param(gcs, 'MakeRTWSettingsObject');
-else
-    % TODO use rtwprivate('get_makertwsettings',gcs,'BuildInfo') - see
-    % R2013a Simulink Coder release notes.
-    makertwObj = coder.internal.ModelCodegenMgr.getInstance;
-end
-makertwArgs = makertwObj.BuildInfo.BuildArgs;
-% Use MLink by default
-downloadMethod = '"MLink"';
-for i=1:length(makertwArgs)
-    if strcmp(makertwArgs(i).DisplayLabel,'MICRODAQ_DOWNLOAD_METHOD')
-        downloadMethod = makertwArgs(i).Value;
+    % Check the MicroDAQ download method
+    if verLessThan('matlab', '8.1')
+        makertwObj = get_param(gcs, 'MakeRTWSettingsObject');
+    else
+        % TODO use rtwprivate('get_makertwsettings',gcs,'BuildInfo') - see
+        % R2013a Simulink Coder release notes.
+        makertwObj = coder.internal.ModelCodegenMgr.getInstance;
+    end
+    makertwArgs = makertwObj.BuildInfo.BuildArgs;
+    for i=1:length(makertwArgs)
+        if strcmp(makertwArgs(i).DisplayLabel,'MICRODAQ_DOWNLOAD_METHOD')
+            downloadMethod = makertwArgs(i).Value;
+        end
     end
 end
+
 
 if strcmp(downloadMethod,'"JTAG"')
     disp('### Using JTAG for download...');
