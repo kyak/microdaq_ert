2d3b04459fdfaee1dcc9f150dd89845ebbaa2214
Skibool <skiba.g@gmail.com>
2013-09-21 11:45:53 +0200

    Missing header for ads8568.c

---
+++ blocks/mdaq/pinmux_config_gpio.h
@@ -0,0 +1,29 @@
+/*
+MUX1 Pins: 
+
+
+MUX2 Pins: 
+
+
+MUX3 Pins: 
+
+
+MUX4 Pins: 
+GP2[12] GP2[15] GP3[9] GP3[10] GP0[8] GP0[9] GP0[10] GP0[11] GP0[12] GP0[13] 
+GP0[14] GP0[15] GP1[0] GP1[3] GP1[4] GP1[5] GP1[6] GP1[7] GP1[8] GP1[10] 
+GP1[12] GP1[13] GP1[14] 
+
+MUX5 Pins: 
+
+*/
+
+/* Created using the Pin Setup Utility (Version: 1.0.663.14) */
+
+
+#define PINMUX9_VALUE  0x18018111
+#define PINMUX11_VALUE 0x11188822
+#define PINMUX14_VALUE 0x88222822
+#define PINMUX15_VALUE 0x28888888
+#define PINMUX16_VALUE 0x88888882
+#define PINMUX17_VALUE 0x00188818
+
