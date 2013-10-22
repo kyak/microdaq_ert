5565ffe9875c6728bfac740f9b209a9989a199b8
kyak <bas@bmail.ru>
2013-10-22 13:46:05 +0400

    This time, really fix it for R2013b

--- microdaq/microdaq_download.m
+++ microdaq/microdaq_download.m
@@ -12,16 +12,17 @@ downloadMethod = '"MLink"';
 if (ischar(makertwObj)) %String 'PIL'
     outfile = modelName;
 else
-    outfile = fullfile(makertwObj.BuildDirectory, [modelName, '.out']);
+    outfile = [modelName, '.out'];
     % Check the MicroDAQ download method
     if verLessThan('matlab', '8.1')
         makertwObj = get_param(gcs, 'MakeRTWSettingsObject');
+        makertwArgs = makertwObj.BuildInfo.BuildArgs;
     else
-        % TODO use rtwprivate('get_makertwsettings',gcs,'BuildInfo') - see
-        % R2013a Simulink Coder release notes.
-        makertwObj = coder.internal.ModelCodegenMgr.getInstance;
+        % See R2013a Simulink Coder release notes.
+        makertwObj = rtwprivate('get_makertwsettings',gcs,'BuildInfo');
+        makertwArgs = makertwObj.BuildArgs;
     end
-    makertwArgs = makertwObj.BuildInfo.BuildArgs;
+
     for i=1:length(makertwArgs)
         if strcmp(makertwArgs(i).DisplayLabel,'MICRODAQ_DOWNLOAD_METHOD')
             downloadMethod = makertwArgs(i).Value;
