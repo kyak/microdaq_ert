315953f7fe3ec0b841bf765640eb5871f7bf2ecb
skibool <skiba.g@gmail.com>
2014-04-12 10:20:58 +0200

    Encoder dir and reset

--- blocks/mdaqenc.c
+++ blocks/mdaqenc.c
@@ -67,6 +67,10 @@ void ENC2Init( void )
 	pru_stop(0); 
 	pru_load(0, (const uint32_t *)pru_encoder, sizeof(pru_encoder) / 4 ); 
 	pru_run(0); 
+
+    /* enable all channels */
+	pru_set_reg(0, 0, 0xffffffff); 
+
 #endif
 }
 
@@ -80,10 +84,91 @@ void ENC2Step(unsigned char reset_ch1,
         int *pos_ch4, unsigned char *dir_ch4)
 {
 #if (!defined MATLAB_MEX_FILE) && (!defined MDL_REF_SIM_TGT)
-	pru_set_reg(0, 0, 0xffffffff); 
+    static int prev_pos_ch1, prev_pos_ch2, prev_pos_ch3, prev_pos_ch4;
+
+	if ( reset_ch1 != 0 ) 
+	{
+	    pru_set_reg(0, 1, 0); 
+		*dir_ch1 = 0; 
+		*pos_ch1 = 0; 
+		prev_pos_ch1 = 0; 
+		return;
+	}
+
+	if ( reset_ch2 != 0 ) 
+	{
+	    pru_set_reg(0, 2, 0); 
+		*dir_ch2 = 0; 
+		*pos_ch2 = 0; 
+		prev_pos_ch2 = 0; 
+		return;
+	}
+
+	if ( reset_ch3 != 0 ) 
+	{
+	    pru_set_reg(0, 3, 0); 
+		*dir_ch3 = 0; 
+		*pos_ch3 = 0; 
+		prev_pos_ch3 = 0; 
+		return;
+	}
+
+	if ( reset_ch4 != 0 ) 
+	{
+	    pru_set_reg(0, 4, 0); 
+		*dir_ch4 = 0; 
+		*pos_ch4 = 0; 
+		prev_pos_ch4 = 0; 
+		return;
+	}
+
 	*pos_ch1 = pru_get_reg(0, 1); 
+	if ( prev_pos_ch1 < *pos_ch1 )  
+	{
+		*dir_ch1 = 1; 
+	}
+	else if (prev_pos_ch1 > *pos_ch1) 
+	{
+		*dir_ch1 = 2; 
+	}
+	else 
+		*dir_ch1 = 0; 
+
 	*pos_ch2 = pru_get_reg(0, 2); 
+	if ( prev_pos_ch2 < *pos_ch2 )  
+	{
+		*dir_ch2 = 1; 
+	}
+	else if (prev_pos_ch2 > *pos_ch2) 
+	{
+		*dir_ch2 = 2; 
+	}
+	else 
+		*dir_ch2 = 0; 
+
 	*pos_ch3 = pru_get_reg(0, 3); 
+	if ( prev_pos_ch3 < *pos_ch3 )  
+	{
+		*dir_ch3 = 1; 
+	}
+	else if (prev_pos_ch3 > *pos_ch3) 
+	{
+		*dir_ch3 = 2; 
+	}
+	else 
+		*dir_ch3 = 0; 
+
 	*pos_ch4 = pru_get_reg(0, 4); 
+	if ( prev_pos_ch4 < *pos_ch4 )  
+	{
+		*dir_ch4 = 1; 
+	}
+	else if (prev_pos_ch4 > *pos_ch4) 
+	{
+		*dir_ch4 = 2; 
+	}
+	else 
+		*dir_ch4 = 0; 
+
 #endif
 }
