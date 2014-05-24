1429d155aae6fef5029e43e4a2b06918dd3852bd
skibool <skiba.g@gmail.com>
2014-05-24 14:41:03 +0200

    Missing header file

--- blocks/mdaqdac.h
+++ blocks/mdaqdac.h
@@ -2,7 +2,7 @@
 #define _MDAQDAC_H
 
 void DACInit( unsigned char converter, unsigned char *channels, unsigned char channel_count, unsigned char update_mode, unsigned char update_mode_tirg );
-void DACStep(double *dac_data, unsigned char channel_count); 
+void DACStep(double *dac_data, unsigned char *channels, unsigned char channel_count); 
 void DACTerminate(float *dac_data_term, unsigned char channel_count, unsigned short term_all_ch); 
 
 #endif /* _MDAQDAC_H */
