f712f7355e7b19b67966f88abe62e08709230bf1
skibool <skiba.g@gmail.com>
2017-01-27 18:46:20 +0100

    Update DSPlib

--- blocks/mdaq/mdaq_ai.h
+++ blocks/mdaq/mdaq_ai.h
@@ -7,7 +7,6 @@
  * of the BSD license.  See the LICENSE file for details.
  */
 
-
 #ifndef MDAQ_AI_H
 #define MDAQ_AI_H
 
@@ -16,7 +15,7 @@
 /* AI channels */
 enum mdaq_ai_ids
 {
-	AI1 = 1,
+    AI1 = 1,
     AI2,
     AI3,
     AI4,
@@ -37,30 +36,40 @@ enum mdaq_ai_ids
 /* AI converters */
 enum mdaq_adc_ids
 {
-	ADC01 = 1,
-	ADC02,
-	ADC03,
-	ADC04,
-	ADC05
+    ADC01 = 1,
+    ADC02,
+    ADC03,
+    ADC04,
+    ADC05,
+    ADC06,
+    ADC07,
+    ADC08,
+    ADC09,
+    ADC10,
+    ADC11,
+    ADC12
 };
+
 
 /* AI range */
 #define AI_10V 		(1 << 0)
 #define AI_5V 		(1 << 1)
 #define AI_2V5  	(1 << 2)
-#define AI_2V   	(1 << 3)
+#define AI_1V25   	(1 << 3)
+#define AI_0V64     (1 << 4)
 
 /* AI polarity */
-#define AI_BIPOLAR 	(1 << 8)
-#define AI_UNIPOLAR	(1 << 9)
+#define AI_BIPOLAR 	(1 << 24)
+#define AI_UNIPOLAR	(1 << 25)
 
 /* AI mode */
-#define AI_SINGLE  	(1 << 16)
-#define AI_DIFF    	(1 << 17)
+#define AI_SINGLE  	(1 << 28)
+#define AI_DIFF    	(1 << 29)
 
-
-int mdaq_ai_init(uint8_t converter, uint32_t range, uint32_t polarity, uint32_t mode);
-int mdaq_ai_config_ch(uint8_t ch[], uint8_t ch_count);
-int mdaq_ai_read(uint8_t ch[], uint8_t ch_count, uint16_t *adc_value, float *value);
+int mdaq_ai_init( uint8_t converter, uint32_t range, uint32_t polarity, uint32_t mode );
+int mdaq_ai_config_ch( uint8_t ch[], uint8_t ch_count );
+int mdaq_ai_read( uint8_t ch[], uint8_t ch_count, uint16_t *adc_value, float *value );
+int mdaq_ai_scan_init(uint8_t ch[], uint8_t ch_count, uint32_t mode, uint32_t scan_count, float freq);
+int mdaq_ai_scan(uint32_t scan_count, uint16_t *adc_value);
 
 #endif /* MDAQ_AI_H */
--- blocks/mdaq/mdaq_ao.h
+++ blocks/mdaq/mdaq_ao.h
@@ -12,29 +12,53 @@
 
 #include <stdint.h>
 
+/* AO channel IDs */
 enum mdaq_ao_ids
 {
-	AO1 = 1,
-	AO2,
-	AO3,
-	AO4,
-	AO5,
-	AO6,
-	AO7,
-	AO8
+    AO1 = 1,
+    AO2,
+    AO3,
+    AO4,
+    AO5,
+    AO6,
+    AO7,
+    AO8,
+    AO9,
+    AO10,
+    AO11,
+    AO12,
+    AO13,
+    AO14,
+    AO15,
+    AO16
 };
 
+/* AO converter IDs */
 enum mdaq_dac_ids
 {
-	DAC01 = 1,
-	DAC02,
-	DAC03
+    DAC01 = 1,
+    DAC02,
+    DAC03,
+    DAC04,
+    DAC05
 };
 
-#define AO_ASYNC		(1 << 1)
-#define AO_SYNC			(1 << 2)
+enum mdaq_ao_range
+{
+    AO_0_TO_5V = 0,
+    AO_0_TO_10V,
+    AO_PLUS_MINUS_5V,
+    AO_PLUS_MINUS_10V,
+    AO_PLUS_MINUS_2V5
+};
+
+/* AO mode */
+#define AO_ASYNC    (1 << 1)
+#define AO_SYNC     (1 << 2)
 
 int mdaq_ao_init( uint8_t converter,  uint8_t mode);
 int mdaq_ao_write( uint8_t ch[], uint8_t ch_count, const float *value);
+int mdaq_ao_ch_config(uint8_t ch[], uint8_t range[], uint8_t ch_count);
+int mdaq_ao_scan_prepare_data(uint8_t ch, uint8_t range, float *value, uint32_t sample_count);
 
 #endif /* MDAQ_AO_H */
